d038ea09bb75f3be5bec130ddc8219ebd7ff629b
drey dreyy <agrosuliac@gmail.com>
2020-05-06 23:54:29 +0000

    json

--- website/config.py
+++ website/config.py
@@ -1,6 +1,10 @@
 # configuration variables for database and email for sending emails to users and also a secret key to protect
 # our app from online hackers
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 
 class Config:
@@ -10,5 +14,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
