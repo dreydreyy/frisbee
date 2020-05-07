6515af7f91f5dae3cdf531c62ad5ff5535d40543
dreydreyy <agrosuliac@gmail.com>
2020-05-07 17:30:24 -0400

    added gitignore

--- website/config.py
+++ website/config.py
@@ -3,8 +3,8 @@
 import os
 import json
 
-with open('/etc/config.json') as config_file:
-    config = json.load(config_file)
+# with open('/etc/config.json') as config_file:
+#  config = json.load(config_file)
 
 
 class Config:
@@ -14,5 +14,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config.get('EMAIL_USER')
-    MAIL_PASSWORD = config.get('EMAIL_PASS')
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
