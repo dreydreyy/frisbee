d63bdb169a31242f024fb9a41e83cf88f6a815ef
dreydreyy <agrosuliac@gmail.com>
2020-05-08 15:07:06 -0400

    fixed max length

--- website/posts/forms.py
+++ website/posts/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length
 
 
 # Normal flaskform with Stringfield and TextArea field and simple validators
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[InputRequired()])
-    content = TextAreaField('Content', validators=[InputRequired()])
+    title = StringField('Title', validators=[InputRequired(), Length(min=1, max=100)])
+    content = TextAreaField('Content', validators=[InputRequired(), Length(min=1, max=400)])
     submit = SubmitField('Post')
